f79f1dde58e5c3720c7a1526bb67a73c02d281fb
David Ooko <davidomondiooko03@gmail.com>
2024-03-27 05:09:30 -0700

    variables

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * main - the entry point of the program
+ * @argc: the argument counter
+ * @argv: the argument passed
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file; /* Pointer var used to represent the file file stream*/
+	char *line = NULL; /* Used to store each line read by the getline() function*/
+	ssize_t values_read; /* Used to store the return value of the getline function*/
+	stack_t *stack = NULL; /* The struct data structure*/
+	unsigned int line_number = 0; /*keeping track of the cureent line of the the file*/
+	size_t len = 0; /* Stores the len of the of each line read by getline()*/
+	char *op_func; /* a pointer variable to store the opcode instructions*/
+
+
+
+
+}
