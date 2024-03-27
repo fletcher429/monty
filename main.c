a7dee3c5970686f118d404abb6fc29ac024515ac
David Ooko <davidomondiooko03@gmail.com>
2024-03-27 06:18:04 -0700

    implemeted main.c

--- main.c
+++ main.c
@@ -14,6 +14,34 @@ int main(int argc, char *argv[])
 	size_t len = 0; /* Stores the len of the of each line read by getline()*/
 	char *op_func; /* a pointer variable to store the opcode instructions*/
 
+	if (argc =! 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	file = fopen(argv[1], "r") /*open the file for read only*/
+
+	if (file == NULL)
+	{
+		fprintf(stderr, " Error: Can't open file <file>\n")
+		exit(EXIT_FAILURE);
+	}
+
+	while ((values_read = getline(&line, &line, file)) != -1)
+	{
+		/**
+		 * perform the opcode functions
+		*/
+
+	}
+
+	free(line);
+	fclose(file);
+	/*Free the stack - do not forget that*/
+	return (EXIT_SUCCESS);
+
 
 
 
