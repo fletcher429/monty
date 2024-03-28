bdaa32b18dcd6a706b80595f67d28a09646dca1c
stephenjarso <stephenjarso6@gmail.com>
2024-03-28 21:55:03 +0300

    pop

--- pop.c
+++ pop.c
@@ -1,23 +1,22 @@
 #include "monty.h"
-
 /**
  * pop - Removes the top element of the stack
  * @stack: Pointer to the stack
  * @line_number: Line number in the file
  */
-void pop(stack_t **stack, unsigned int line_number)
-{
-    stack_t *temp;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+void pop(stack_t **stack, unsigned int line_number) {
+  stack_t *temp;
 
-    temp = *stack;
-    *stack = (*stack)->next;
-    if (*stack)
-        (*stack)->prev = NULL;
-    free(temp);
+  if (*stack == NULL) {  // Check for empty stack using short-circuit evaluation
+    fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+
+  temp = *stack;
+  *stack = temp->next;
+  if (*stack) {
+    (*stack)->prev = NULL;
+  }
+  free(temp);
 }
