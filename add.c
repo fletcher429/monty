c921c5199442ae7a9932b1d1e7c23f8f5a11190d
David Ooko <davidomondiooko03@gmail.com>
2024-03-28 14:37:29 -0700

    add elements - top two elements

--- add.c
+++ add.c
@@ -2,14 +2,14 @@
 /**
  * add - adds the top two elemnts of the stack
  * @stack: pointer to the head of the stack
- * @line_number
+ * @line_number: line number of the opcode
  */
 void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
