bb683ed78b196f496420dae51b485deaed442e7a
David Ooko <davidomondiooko03@gmail.com>
2024-03-28 14:41:01 -0700

    do not touch

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - do nothing to the stack
+ * @stack: pointer ti the head of the stack
+ * @line_number - line of the opcode
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
